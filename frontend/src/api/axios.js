23b2833033dc5ae8bc6bb10f69c29d1fafe86777
VYOM PATEL <124497900+Vyom1912@users.noreply.github.com>
2026-06-26 18:27:00 +0530

    update render url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -5,7 +5,8 @@ import axios from "axios";
 // httpOnly cookies (accessToken, refreshToken) with every request.
 // Without this, the backend auth middleware would never see the tokens.
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+baseURL  :"https://publishpro-a-blogging-platform-backend.onrender.com",
   withCredentials: true, // IMPORTANT: sends cookies cross-origin
 });
 
