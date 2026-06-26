e08a7a852276d7b22fabfe4e09c72b8c953d58dd
VYOM PATEL <124497900+Vyom1912@users.noreply.github.com>
2026-06-26 18:50:02 +0530

    Update API baseURL to use environment variable

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -6,7 +6,8 @@ import axios from "axios";
 // Without this, the backend auth middleware would never see the tokens.
 const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-baseURL: "https://publishpro-a-blogging-platform-backend.onrender.com/api",
+// baseURL: "https://publishpro-a-blogging-platform-backend.onrender.com/api",
+   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // IMPORTANT: sends cookies cross-origin
 });
 
